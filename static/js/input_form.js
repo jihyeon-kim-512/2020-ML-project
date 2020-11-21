5122eb9b9c391870d015dff1eb136354b9909b25
jihyeon-kim-512 <jihyeon.kim.512@gmail.com>
2020-11-21 16:10:19 +0900

    직접 입력 및 수정 폼

---
+++ static/js/input_form.js
@@ -0,0 +1,18 @@
+function categoryChange(e) {
+	var categorize_a = ["의류", "뷰티", "생활비", "여행", "교통비", "식비", "기타"];
+	var categorize_b = ["용돈", "월급", "기타"];
+	var target = document.getElementById("categorize");
+
+	if(e.value == "a") var d = categorize_a;
+	else if(e.value == "b") var d = categorize_b;
+	else if(e.value == "c") var d = categorize_c;
+
+	target.options.length = 0;
+
+	for (x in d) {
+		var opt = document.createElement("option");
+		opt.value = d[x];
+		opt.innerHTML = d[x];
+		target.appendChild(opt);
+	}
+}

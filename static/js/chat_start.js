1063d3a1bc1a4632ffcd09d788bd771e489e808e
jihyeon-kim-512 <jihyeon.kim.512@gmail.com>
2020-11-25 15:09:50 +0900

    기본정보 입력페이지 수정

--- static/js/chat_start.js
+++ static/js/chat_start.js
@@ -1,13 +1,27 @@
 $( document ).ready(function() {
   $("#join_btn").on('click', function(e) {
     var name = document.getElementById('name').value;
+    var ERROR_NO_VALUE_NAME = "ERROR_NO_VALUE_NAME";
     var ssn = document.getElementById('ssn').value;
     var select = document.getElementById('select').value;
     var phone = document.getElementById('phone').value;
+    if (name == '' || ssn == '' || phone == '') {
+        alert("회원가입정보를 입력해주세요!")
+    }
+    else if (ssn != '' && ssn.toString().length != 6) {
+        alert("주민등록번호 자리수를 확인해주세요!")
+    }
+    else if (phone != '' && phone.toString().length != 11) {
+        alert("전화번호 자리수를 확인해주세요!")
+    }
+
+    else alert(name);
   });
 });
 
-$('.terms_of_use_btn').on('click', funsction(e) {
+
+
+$('.terms_of_use_btn').on('click', function(e) {
   e.preventDefault();
   var el = $($(this).attr('href'));
   if (!el.hasClass('open')) {
@@ -19,4 +33,9 @@ $('.terms_of_use_btn').on('click', funsction(e) {
 
 $('.btn_popup_close').on('click', function(e) {
   $(this).closest('.Popup_page').removeClass('open');
+});
+
+
+$( document ).ready(function() {
+
 });
